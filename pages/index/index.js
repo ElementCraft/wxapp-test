ff4a55ac0a51581b7685b092c4e4ca6afbb1f67c
ElementCraft <976228284@qq.com>
2018-06-06 14:38:17 +0800

    fix

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,21 +5,26 @@ const app = getApp()
 Page({
 	data: {
 		scrollTop: 100,
-		newestArticles:[],
-		hotArticles: []
+		newestArticles:[],	// 最新文章
+		hotArticles: []		// 热门文章
 	},
+	// 调整接口获取到的文章数据
 	fixArticlesInfo: (articles) => {
 		articles.map((obj,i) => {
 			let icon = obj.userColumn.user.iconPath;
 
+			// 用户有头像显示用户头像，没有显示默认头像
 			obj.iconPath = icon ? app.url + icon : "/images/default_icon.png"; 
+			// 计算文章发表时间已过多久
 			obj.gmtCreate = util.CountAgo(obj.gmtCreate);
+			
 			return obj;
 		});
 
 		return articles;
 	},
 	onLoad: function () {
+		// 请求最新文章信息
 		wx.request({
 			url: app.url + "api/article/all",
 			data: {
@@ -37,6 +42,7 @@ Page({
 			}
 		});
 
+		// 请求热门文章信息
 		wx.request({
 			url: app.url + "api/article/hot",
 			data: {
